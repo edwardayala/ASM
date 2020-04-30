d985e8edaf8253a60076111c3c888b2450718a07
edwardayala <47757464+edwardayala@users.noreply.github.com>
2020-04-30 10:53:02 -0500

    Works for most numbers - maybe some don't work

--- assignment.asm
+++ assignment.asm
@@ -1,17 +1,66 @@
 ;----------------------------------------------------------------------------+
 TITLE assignment                                                             |
-;                                                                            |
+; Write a program that will prompt the user to enter a number.               |
+; The program will determine whether or not the number is prime.             |
+; Recall, a number is prime if it is only divisible by 1 and itself.         |
+; Your output for each run of the program should be either:                  |
+; "The number X is prime" or                                                 |
+; "The number X is NOT prime".                                               |
 ;                                                                            |
 ;----------------------------------------------------------------------------+
 
 INCLUDE Irvine32.inc
 
 .data
-
+input   DWORD   ?
+prompt  BYTE    "Enter a number:",0
+output  BYTE    "The number ",0
+isPrime BYTE    " is prime.",0dh,0ah,0
+ntPrime BYTE    " is NOT prime.",0dh,0ah,0
+tstNum1 DWORD   2
+tstNum2 DWORD   3
+; TEST NUMBERS: 2, 3, 5, 7, 11, & 13
 .code
 main PROC
+  mov   EDX, OFFSET prompt
+  call  WriteString
+  call  ReadInt
 
-  call DumpRegs
+  mov   input, EAX
+
+  ; DIVISION
+
+  .REPEAT
+    mov   EBX, 2
+    mov   EDX, 0
+    mov   EAX, input
+    mov   ECX, EBX
+    div   ECX
+	inc   EBX
+	cmp   EDX, 0
+	je    NOTPRIME
+	jg    PRIME
+  .UNTIL EBX == input-1
+
+  NOTPRIME:
+  mov   EDX, OFFSET output
+  call  WriteString
+  mov   EAX, input
+  call  WriteDec
+  mov   EDX, OFFSET ntPrime
+  call  WriteString
+  jmp   QUIT
+
+  PRIME:
+  mov   EDX, OFFSET output
+  call  WriteString
+  mov   EAX, input
+  call  WriteDec
+  mov   EDX, OFFSET isPrime
+  call  WriteString
+  jmp   QUIT
+
+  QUIT:
   exit
 main ENDP
 END main
