cb95f109cb441832dc02a9074d5920cc067a331d
edwardayala <47757464+edwardayala@users.noreply.github.com>
2020-05-07 10:34:34 -0500

    last q

--- assignment.asm
+++ assignment.asm
@@ -1,5 +1,5 @@
 ;----------------------------------------------------------------------------+
-TITLE final                                                                  |
+TITLE assignment                                                             |
 ;                                                                            |
 ;                                                                            |
 ;----------------------------------------------------------------------------+
@@ -7,11 +7,33 @@ TITLE final                                                                  |
 INCLUDE Irvine32.inc
 
 .data
-
+mInput BYTE "Input a value for M:",0
+nInput BYTE "Input a value for N:",0
 .code
 main PROC
+	mov EDX, OFFSET mInput
+	call WriteString
+	call ReadInt
+	mov EBX, EAX
 
-  call DumpRegs
-  exit
+	mov EDX, OFFSET nInput
+	call WriteString
+	call ReadInt
+
+	call BetterRandomRange
+
+    exit
 main ENDP
+BetterRandomRange PROC
+mov ECX, 50
+L1:
+	call RandomRange
+	.IF EAX < EBX
+		add EAX, EBX
+	.ENDIF
+	call WriteDec
+	call Crlf
+	loop L1
+ret
+BetterRandomRange ENDP
 END main
